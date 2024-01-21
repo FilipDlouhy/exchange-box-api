06564ce59ab85b8a27b056609622459c241d119b
swpv <80281721+Rajce23@users.noreply.github.com>
2024-01-21 20:56:55 +0100

    enabeld cross origin

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
 
   app.use(cookieParser());
+  app.enableCors({ origin: true });
 
   await app.listen(3000);
 }
